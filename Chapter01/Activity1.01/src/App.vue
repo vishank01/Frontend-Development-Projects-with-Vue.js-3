508934b966e37f6949223b6292a2302e4748cea1
vishnums <shashankmv01@gmail.com>
2023-07-15 13:30:58 +0530

    exercise1-1 my practice changes

--- Chapter01/Activity1.01/src/App.vue
+++ Chapter01/Activity1.01/src/App.vue
@@ -1,131 +1,23 @@
 <template>
-  <div class="container">
-    <h2>Shopping list</h2>
-    <div class="user-input">
-      <input
-        placeholder="Press enter to add new item"
-        v-model="input"
-        @keyup.enter="addItem"
-        ref="input"
-      />
-      <button @click="addItem">Add item</button>
-    </div>
-
-    <ul v-if="shoppingList">
-      <li v-for="(item, i) in shoppingList" :key="i" class="item">
-        <span>{{ item }}</span>
-        <button class="button--remove" @click="deleteItem(i)">Remove</button>
-      </li>
-    </ul>
-    <br />
-    <button class="button--delete" @click="deleteAll()">Delete all</button>
-  </div>
+  <Exercise />
+  <p>{{ text }}</p>
 </template>
-
-<script>
+<script setup>
+  import Exercise from "./components/Exercise1-01.vue"
+  const text = 'This is sample constant text'
+</script>
+<!-- <script>
+import Exercise from "./components/Exercise1-01.vue";
 export default {
+  components: {
+    Exercise,
+  },
   data() {
     return {
-      input: "",
-      shoppingList: [],
+      text: "Hello from Data",
     };
   },
-  methods: {
-    addItem() {
-      // Don't allow adding to the list if empty
-      if (!this.input) return;
-      this.shoppingList.push(this.input);
-      // Clear the input after adding
-      this.input = "";
-      // Focus the input element again for quick typing!
-      this.$refs.input.focus();
-    },
-    deleteItem(i) {
-      if (i < 0 || i > this.shoppingList.length) return;
-
-      this.shoppingList = this.shoppingList.filter(
-        (item, index) => index !== i
-      );
-    },
-    deleteAll() {
-      this.shoppingList = [];
-    },
-  },
-};
-</script>
-
+}; -->
+<!-- </script> -->
 <style>
-.container {
-  max-width: 600px;
-  margin: 80px auto;
-}
-
-.item {
-  display: flex;
-  justify-content: space-between;
-}
-
-// Type
-.h2 {
-  font-size: 21px;
-}
-
-.user-input {
-  display: flex;
-  align-items: center;
-  padding-bottom: 20px;
-  input {
-    width: 100%;
-    padding: 10px 6px;
-    margin-right: 10px;
-  }
-}
-
-.item {
-  display: flex;
-  align-items: center;
-}
-
-// Buttons
-button {
-  appearance: none;
-  padding: 10px;
-
-  font-weight: bold;
-  border-radius: 10px;
-  border: none;
-  background: $color-grey;
-  color: white;
-  white-space: nowrap;
-
-  + button {
-    margin-left: 10px;
-  }
-}
-
-.button--delete {
-  display: block;
-  margin: 0 auto;
-  background: red;
-}
-
-.button--remove {
-  background: none;
-  color: red;
-  text-transform: uppercase;
-  font-size: 11px;
-  align-self: flex-end;
-}
-
-ul {
-  display: block;
-  margin: 0 auto;
-  padding: 30px;
-  border: 1px solid rgba(0, 0, 0, 0.25);
-
-  > li {
-    color: $color-grey;
-    margin-bottom: 4px;
-  }
-}
 </style>
---
+++ Chapter01/Activity1.01/src/components/Exercise1-01.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>My First Component!</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+h1 {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
